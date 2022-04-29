6f3fd916f804e161ea72444555d203af3851bf90
MRDGH2821 <mihirrabade@gmail.com>
2022-04-29 22:01:32 +0530

    feat: extend types of dotenv

--- src/interfaces/types.d.ts
+++ src/interfaces/types.d.ts
@@ -1,8 +1,24 @@
-export type UseEnvOptions = {
-  path?: string;
-  encoding?: string;
-  ignoreProcessEnv?: false;
+import { DotenvConfigOptions } from 'dotenv';
+import { DotenvExpandOptions } from 'dotenv-expand';
+
+export interface UseEnvOptions
+  extends DotenvConfigOptions,
+  DotenvExpandOptions {
+  /**
+	 * Default: `true`
+	 *
+	 * Use dotenv-expand to expand the env variables
+	 */
   enableExpand?: true;
-  debug?: boolean;
-  override?: boolean;
-};
+
+  /**
+	 * Default: `true`
+	 *
+	 * Update .gitignore file to ignore folder created by use-env
+	 * This is best left as "true".
+	 *
+	 * WARNING!!
+	 * Change this option only if you really really know what you are doing.
+	 */
+  updateGitIgnore?: boolean;
+}
