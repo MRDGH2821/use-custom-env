7cae0113b05b14df426c6ad469c9bd542aa554e1
MRDGH2821 <mihirrabade@gmail.com>
2022-04-30 00:18:48 +0530

    chore: update test cases

--- test/index.ts
+++ test/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import { useEnv } from '../src';
@@ -9,18 +10,21 @@ envSuite('should be a function', () => {
 });
 
 envSuite('should default to env when no input given', () => {
-  useEnv('env', { debug: true });
+  console.log('||---No Options given-----------');
+  useEnv();
   assert.is(process.env.DEFAULT, 'working in default options mode');
 });
 
 envSuite('should make test folder & load env', () => {
+  console.log('||---Load env from test folder-----------');
   useEnv('test');
   assert.is(process.env.TEST1, 'one');
   assert.is(process.env.TEST2, 'two');
 });
 
 envSuite('should be able to load multiline env', () => {
-  useEnv('test');
+  console.log('||---Multiline load-----------');
+  useEnv('multiline');
   const multiline = `---this is a multi
 line test for use-env
 and that's it---`;
@@ -28,7 +32,8 @@ and that's it---`;
 });
 
 envSuite('should be able to expand env', () => {
-  useEnv('test');
+  console.log('||---Env Expand-----------');
+  useEnv('expand');
 
   assert.is(process.env.TEST3, 'one two');
 });
